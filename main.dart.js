126ac4cbc571022042a2b2c3575654065e2bcd72
Reynaldo Colon <ReynaldoColon@gmail.com>
2026-04-08 13:41:27 -0400

    Updated the UI and colors

--- main.dart.js
+++ main.dart.js
@@ -56615,7 +56615,7 @@ return B.c.aC(r,s)},
 $S:370}
 A.A9.prototype={
 O(a){var s=null
-return new A.r0(B.CQ,"Flutter Demo",A.a5G(s,A.a7I(B.L,s,s,B.CC),s),s)}}
+return new A.r0(B.CQ,"Reynaldo Colon Home Page",A.a5G(s,A.a7I(B.L,s,s,B.CC),s),s)}}
 A.rd.prototype={
 al(){return new A.vl()}}
 A.vl.prototype={
@@ -56623,7 +56623,7 @@ R3(){this.aI(new A.a0o(this))},
 O(a){var s,r=null,q=A.aD(a).ax,p=q.y2
 q=p==null?q.c:p
 this.a.toString
-p=A.aa7("Flutter Demo Home Page",r)
+p=A.aa7("Reynaldo Colon Home Page",r)
 s=A.xP(A.ag5(A.d([B.Kh,A.aa7(""+this.d,A.aD(a).ok.e)],t.nA),B.oU),r,r)
 return new A.tg(new A.oU(p,q,new A.GA(r,r,1/0,56),r),s,new A.za(B.yT,"Increment",this.gR2(),B.Lw,r),r)}}
 A.a0o.prototype={
